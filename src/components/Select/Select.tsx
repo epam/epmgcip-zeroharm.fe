0c023c052f33ef55e5c053a17cf5ec4d906007ad
Marina Khitrovo <Marina_Khitrovo@epam.com>
2023-06-17 00:02:50 +0500

    one default address on home page

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -11,19 +11,17 @@ import { useTranslationValues } from "@/hooks/useTranslationValues";
 export const Select: React.FC = () => {
   const { location, setLocation } = useDataStore();
   const locations = useTranslationValues("locations");
-
   const placeHolder = location || "Select location";
+
   return (
-    <InputGroup mt="8px">
-      <InputLeftElement
-        pointerEvents="none"
-        color="gray.400"
-        children={<IconMapPoint />}
-      />
+    <InputGroup mt="8px" pointerEvents="none">
+      <InputLeftElement color="gray.400" children={<IconMapPoint />} />
       <ChakraSelect
         placeholder={placeHolder}
         onChange={(e) => setLocation(e.target.value)}
         focusBorderColor="white"
+        iconSize={"1"}
+        defaultValue={locations[0]}
       >
         {locations?.map((location) => (
           <option key={location} value={location}>
