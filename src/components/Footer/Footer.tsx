c66e7a81e6ff1e979154374229cb106264481b0a
Marina Khitrovo <Marina_Khitrovo@epam.com>
2023-07-11 16:44:44 +0500

    add translations for footer en, ru

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
 import { Flex, Text, Link, Button } from "@chakra-ui/react";
+import { t } from "i18next";
 
 const Footer: React.FC = () => {
   return (
     <Flex p="4px" gap="32px" w="100%" justify="space-between">
-      <Text fontSize='xs'>
-        All data is provided in collaboration with the{" "}
-        <Link  href="https://hydromet.uz/" isExternal>
-          Center of Hydrometeorological Service of the Republic of Uzbekistan
-          (Uzhydromet){" "}
+      <Text fontSize="xs">
+        {t("pages.footer.text")} {" "}
+        <Link href="https://hydromet.uz/" isExternal>
+          {t("pages.footer.link")} {" "}
         </Link>
       </Text>
-      <Button size="xs">Feedback</Button>
+      <Button size="xs">{t("pages.footer.button")}</Button>
     </Flex>
   );
 };
