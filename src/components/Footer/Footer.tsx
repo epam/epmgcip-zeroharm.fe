8bbc2340d2673ce7e99bdddcdbbdd27685cf0158
Marina Khitrovo <Marina_Khitrovo@epam.com>
2023-07-11 16:35:54 +0500

    implement link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
-import { Flex, Button } from "@chakra-ui/react";
+import { Flex, Text, Link, Button } from "@chakra-ui/react";
 
 const Footer: React.FC = () => {
   return (
-    <Flex p="4px" gap="32px" w="100%">
-      <Button size='xs'>Feedback</Button>
+    <Flex p="4px" gap="32px" w="100%" justify="space-between">
+      <Text fontSize='xs'>
+        All data is provided in collaboration with the{" "}
+        <Link  href="https://hydromet.uz/" isExternal>
+          Center of Hydrometeorological Service of the Republic of Uzbekistan
+          (Uzhydromet){" "}
+        </Link>
+      </Text>
+      <Button size="xs">Feedback</Button>
     </Flex>
   );
 };
