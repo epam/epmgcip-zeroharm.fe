2a6e911524552095b2324a8f13e91bb1037a80e5
Marina Khitrovo <Marina_Khitrovo@epam.com>
2023-07-13 11:40:47 +0500

    change styles to fit rus text

--- src/components/BaseSelect/BaseSelect.tsx
+++ src/components/BaseSelect/BaseSelect.tsx
@@ -1,89 +1,99 @@
 import React from "react";
 import { Select } from "chakra-react-select";
 
-const getSelectComponentStyleConfig = (styles: any) => (provided: any, state: any) => {
-  const stylesWithState = Object.entries(styles).filter(([_, value]) => typeof value === "function");
-
-  if (stylesWithState?.length) {
-    const resolvedStyles = stylesWithState.map(([key, func]: [string, any]) => ([
-        key,
-        func(state)
-      ])
+const getSelectComponentStyleConfig =
+  (styles: any) => (provided: any, state: any) => {
+    const stylesWithState = Object.entries(styles).filter(
+      ([_, value]) => typeof value === "function"
     );
-    const componentStyles = resolvedStyles.reduce((acc, [key, val]) => Object.assign(acc, { [key] : val }), {});
+
+    if (stylesWithState?.length) {
+      const resolvedStyles = stylesWithState.map(
+        ([key, func]: [string, any]) => [key, func(state)]
+      );
+      const componentStyles = resolvedStyles.reduce(
+        (acc, [key, val]) => Object.assign(acc, { [key]: val }),
+        {}
+      );
+
+      return {
+        ...provided,
+        ...styles,
+        ...componentStyles,
+      };
+    }
 
     return {
       ...provided,
       ...styles,
-      ...componentStyles
     };
-  }
-
-  return {
-    ...provided,
-    ...styles
   };
-};
 
 const BaseSelectStyleConfig = {
   container: {
     bg: "gray.700",
-    zIndex: "2"
+    zIndex: "2",
   },
   menuList: {
     bg: "gray.700",
   },
   option: {
-    bg: (state: any) => state.isFocused ? "gray.500" : "gray.700",
+    bg: (state: any) => (state.isFocused ? "gray.500" : "gray.700"),
   },
   indicatorSeparator: {
-    display: "none"
+    display: "none",
   },
   dropdownIndicator: {
-    bg: "ray.700"
-  }
+    bg: "ray.700",
+  },
 };
 
 type OptionT = {
-  label: string,
-  value: string
-}
+  label: string;
+  value: string;
+};
 
 type SelectPropsT = {
-  options: Array<OptionT>,
-  defaultValue: string,
-  styles?: object,
-  isDisabled?: boolean,
-  onChange?: (lang: any) => void
-}
+  options: Array<OptionT>;
+  defaultValue: string;
+  styles?: object;
+  isDisabled?: boolean;
+  onChange?: (lang: any) => void;
+};
 
-export const  BaseSelect: React.FC<SelectPropsT> = (props: SelectPropsT) => {
-  const {
-    defaultValue,
-    options,
-    styles,
-    isDisabled,
-    onChange
-  } = props;
-  const val = options.find(({ label, value }) => label === defaultValue || value === defaultValue);
+export const BaseSelect: React.FC<SelectPropsT> = (props: SelectPropsT) => {
+  const { defaultValue, options, styles, isDisabled, onChange } = props;
+  const val = options.find(
+    ({ label, value }) => label === defaultValue || value === defaultValue
+  );
 
   const mergeStyleConfigs = (newStyles: any) => {
     const styles: any = Object.assign({}, BaseSelectStyleConfig);
 
-    return Object.keys(newStyles).reduce((acc, key: string) => ({
-      ...acc,
-      [key]: key in styles ? {
-        ...styles[key],
-        ...newStyles[key]
-      } : {
-        ...newStyles[key]
-      }
-    }), styles);
+    return Object.keys(newStyles).reduce(
+      (acc, key: string) => ({
+        ...acc,
+        [key]:
+          key in styles
+            ? {
+                ...styles[key],
+                ...newStyles[key],
+              }
+            : {
+                ...newStyles[key],
+              },
+      }),
+      styles
+    );
   };
 
-  const selectStyles = Object.entries(mergeStyleConfigs(styles)).reduce((acc, [componentName, newStyles]) => Object.assign(acc, {
-    [componentName]: getSelectComponentStyleConfig(newStyles)
-  }), {});
+  const selectStyles = Object.entries(mergeStyleConfigs(styles)).reduce(
+    (acc, [componentName, newStyles]) =>
+      Object.assign(acc, {
+        [componentName]: getSelectComponentStyleConfig(newStyles),
+      }),
+    {}
+  );
 
   return (
     <Select
