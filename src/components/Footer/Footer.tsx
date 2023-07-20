8ee6b0b5b7ceb25a421aee85c7d3882ba4944262
Marina Khitrovo <Marina_Khitrovo@epam.com>
2023-07-20 12:36:47 +0500

    add link to feedback button

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,20 @@
 import * as React from "react";
-import { Flex, Text, Link, Button } from "@chakra-ui/react";
+import { Flex, Text, Link as ChakraLink, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import { t } from "i18next";
 
 const Footer: React.FC = () => {
   return (
     <Flex p="4px" gap="32px" w="100%" justify="space-between">
       <Text fontSize="xs">
-        {t("pages.footer.text")} {" "}
-        <Link href="https://hydromet.uz/" isExternal>
-          {t("pages.footer.link")} {" "}
-        </Link>
+        {t("pages.footer.text")}{" "}
+        <ChakraLink href="https://hydromet.uz/" isExternal>
+          {t("pages.footer.link")}{" "}
+        </ChakraLink>
       </Text>
-      <Button size="xs">{t("pages.footer.button")}</Button>
+      <Link to="/form">
+        <Button size="xs">{t("pages.footer.button")}</Button>
+      </Link>
     </Flex>
   );
 };
