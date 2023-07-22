cd987b4f9fdd6e84ceb0469f1076172f03709e5c
Marina Khitrovo <Marina_Khitrovo@epam.com>
2023-07-22 23:49:07 +0500

    feat: modal for feedbackform

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,24 @@
 import * as React from "react";
-import { Flex, Text, Link as ChakraLink, Button } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import {
+  Flex,
+  Text,
+  Link as ChakraLink,
+  Button,
+  useDisclosure,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
 import { t } from "i18next";
+import Form from "../Form/Form";
 
 const Footer: React.FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Flex p="4px" gap="32px" w="100%" justify="space-between">
       <Text fontSize="xs">
@@ -12,9 +27,24 @@ const Footer: React.FC = () => {
           {t("pages.footer.link")}{" "}
         </ChakraLink>
       </Text>
-      <Link to="/form">
-        <Button size="xs">{t("pages.footer.button")}</Button>
-      </Link>
+      <Button size="xs" onClick={onOpen}>
+        {t("pages.footer.button")}
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader w={"initial"} pos={"initial"}>Help us be better</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Form />
+          </ModalBody>
+          <ModalFooter w={"initial"} pos={"initial"}>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Send Feedback
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 };
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,6 +2,5 @@ import PageAbout from "./About";
 import PageHome from "./Home";
 import Page404 from "./404";
 import PageMap from "./Map";
-import PageForm from "./Form";
 
-export { Page404, PageHome, PageAbout, PageMap, PageForm };
+export { Page404, PageHome, PageAbout, PageMap };
