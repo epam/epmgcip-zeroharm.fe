1a15fdcf90c6f615e3a3dacd0d2e486e0ae7f0ac
Marina Khitrovo <Marina_Khitrovo@epam.com>
2023-07-29 22:55:36 +0500

    feat date localization

--- src/helpers/getDate.ts
+++ src/helpers/getDate.ts
@@ -1,5 +1,15 @@
 import dayjs from "dayjs";
+import "dayjs/locale/ru";
+import "dayjs/locale/uz-latn";
+import i18next from "i18next";
 
 export const getDate = (): string => {
-  return dayjs().format("HH:mm, MMM D");
+  switch (i18next.language) {
+    case "ru":
+      return dayjs().locale("ru").format("HH:mm, D MMM");
+    case "uz":
+      return dayjs().locale("uz-latn").format("HH:mm, D MMM");
+    default:
+      return dayjs().locale("en").format("HH:mm, MMM D");
+  }
 };
