b216505b5bfa4ec4aea7159871df8c1d30473ca7
Marina Khitrovo <Marina_Khitrovo@epam.com>
2023-08-16 21:07:38 +0500

    add  icons

---
+++ src/pages/Test.tsx
@@ -0,0 +1,11 @@
+import { Notification } from "@/_UI/Notification/Notification";
+
+const PageTest = () => {
+  return (
+    <>
+      <Notification />
+    </>
+  );
+};
+
+export default PageTest;
