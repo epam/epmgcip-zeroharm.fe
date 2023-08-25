3c9c69d5c65a5c094047d5c6cebed8375c5e329c
Oryngali <oryngali96@gmail.com>
2023-08-25 18:02:56 +0500

    refactor: add some minor changes

--- src/components/LanguageMenu/LanguageMenu.tsx
+++ src/components/LanguageMenu/LanguageMenu.tsx
@@ -11,7 +11,7 @@ import { ReactComponent as RuFlagIcon } from "@/assets/icons/flags/harm-lang-fla
 import { ReactComponent as EnFlagIcon } from "@/assets/icons/flags/harm-lang-flag-united-states.svg";
 import { ReactComponent as UzFlagIcon } from "@/assets/icons/flags/harm-lang-flag-uzbekistan.svg";
 
-const LanguageFlags: Record<string, React.FunctionComponent<React.SVGProps<SVGSVGElement>>> = {
+const LanguagesFlagIcons: Record<string, React.FunctionComponent<React.SVGProps<SVGSVGElement>>> = {
   en: EnFlagIcon,
   ru: RuFlagIcon,
   uz: UzFlagIcon
@@ -26,92 +26,92 @@ export const LanguageMenu: FC = () => {
 
   const languagesOptions = languagesData.map((languageData) => resolveTranslationPath(languageData));
 
-  const CurrentLanguageIcon = LanguageFlags[i18n.language];
+  const CurrentLanguageFlagIcon = LanguagesFlagIcons[i18n.language];
 
   return (
     <Menu autoSelect={false}>
       { ({ isOpen }) => {
         const isOpenOnMobile = isOpen && isMobileWidth;
 
-          return (
-            <>
-              <MenuButton
-                zIndex={2}
-                h={{
-                  base: isOpen ? 14 : "auto",
-                  md: "auto"
-                }}
-                pos={{
-                  base: isOpen ? "fixed" : "static",
-                  md: "static"
-                }}
-                top={0}
-                left={0}
-                pl={{
-                  base: isOpen ? 0 : 4,
-                  md: 4
-                }}
-                w={{
-                  base: isOpen ? "56px" : "76px",
-                  md: "104px"
+        return (
+          <>
+            <MenuButton
+              zIndex={2}
+              h={{
+                base: isOpen ? 14 : "auto",
+                md: "auto"
+              }}
+              pos={{
+                base: isOpen ? "fixed" : "static",
+                md: "static"
+              }}
+              top={0}
+              left={0}
+              pl={{
+                base: isOpen ? 0 : 4,
+                md: 4
+              }}
+              w={{
+                base: isOpen ? "56px" : "76px",
+                md: "104px"
+              }}
+            >
+              <HStack
+                spacing={{
+                  base: isOpenOnMobile ? 0 : 1.5,
+                  md: 2
                 }}
               >
-                <HStack
-                  spacing={{
-                    base: isOpenOnMobile ? 0 : 1.5,
-                    md: 2
+                {
+                  isOpenOnMobile
+                    ? <HStack w={14} justify="center">
+                        <LeftArrowIcon width={20} color="initial" />
+                      </HStack>
+                    : <CurrentLanguageFlagIcon />
+                }
+
+                <Text
+                  display={{
+                    base: isOpen ? "block" : "none",
+                    md: "block"
                   }}
+                  color="initial"
                 >
-                  {
-                    isOpenOnMobile
-                      ? <HStack w={14} justify="center">
-                          <LeftArrowIcon width={20} color="initial" />
-                        </HStack>
-                      : <CurrentLanguageIcon />
-                  }
+                  { !isOpenOnMobile && i18n.language }
+                </Text>
 
-                  <Text
-                    display={{
-                      base: isOpen ? "block" : "none",
-                      md: "block"
-                    }}
+                  <Icon
+                    as={isOpen ? ArrowUpIcon : ArrowDownIcon}
+                    width={4}
                     color="initial"
-                  >
-                    { !isOpenOnMobile &&i18n.language }
-                  </Text>
+                    display={isOpenOnMobile ? "none" : "block"}
+                  />
+              </HStack>
+            </MenuButton>
 
-                    <Icon
-                      as={isOpen ? ArrowUpIcon : ArrowDownIcon}
-                      width={4}
-                      color="initial"
-                      display={isOpenOnMobile ? "none" : "block"}
-                    />
-                </HStack>
-              </MenuButton>
-
-              <MenuList
-                minW={{
-                  base: "100vw",
-                  md: 44,
-                  lg: 60
-                }}
-                paddingTop={isOpenOnMobile ? 14 : 0}
-                minH={{
-                  base: isOpen ? "100vh" : 0,
-                  md: 0
-                }}
-                borderRadius={{
-                  base: 0,
-                  md: 8
-                }}
-                position={{
-                  base: isOpen ? "relative" : "static",
-                  md: "static"
-                }}
-                top={-16}
-                left={0}
-              >
-                { isOpenOnMobile &&
+            <MenuList
+              minW={{
+                base: "100vw",
+                md: 44,
+                lg: 60
+              }}
+              paddingTop={isOpenOnMobile ? 14 : 0}
+              minH={{
+                base: isOpen ? "100vh" : 0,
+                md: 0
+              }}
+              borderRadius={{
+                base: 0,
+                md: 8
+              }}
+              position={{
+                base: isOpen ? "relative" : "static",
+                md: "static"
+              }}
+              top={-16}
+              left={0}
+            >
+              { isOpenOnMobile &&
                   <Flex
                     w="calc(100vw - 56px)"
                     h={14}
@@ -125,29 +125,28 @@ export const LanguageMenu: FC = () => {
                   >
                     Language
                   </Flex>
-                }
-                {
-                  languagesOptions.map(({ languageId, languageName}) => {
-                    const LanguageIcon = LanguageFlags[languageId];
+              }
+              {
+                languagesOptions.map(({ languageId, languageName}) => {
+                  const LanguageFlagIcon = LanguagesFlagIcons[languageId];
 
-                    return (
-                      <MenuItem
-                        key={languageId}
-                        onClick={() => setLanguage(languageId)}
-                        aria-selected={languageId === i18n.language}
-                      >
-                        <LanguageIcon />
+                  return (
+                    <MenuItem
+                      key={languageId}
+                      onClick={() => setLanguage(languageId)}
+                      aria-selected={languageId === i18n.language}
+                    >
+                      <LanguageFlagIcon />
 
-                        { languageName }
-                      </MenuItem>
-                    );
-                  })
-                }
-              </MenuList>
-            </>
-          );
-        }
-      }
+                      { languageName }
+                    </MenuItem>
+                  );
+                })
+              }
+            </MenuList>
+          </>
+        );
+      } }
     </Menu>
   );
 };
