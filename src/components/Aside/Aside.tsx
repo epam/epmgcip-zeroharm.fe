11d79c386618cb7817fb41ef524075942662a38e
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-08-30 20:42:39 +0600

    [FE-174]: move Cards and Indicators into TabPanel

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -1,14 +1,7 @@
 import { Flex } from "@chakra-ui/react";
-import { Tabs, IndexDate, Indicators, LocationSelect } from "@Components";
-import { Cards } from "@UI";
-import { useParameterData } from "@Hooks";
-import { tabsData } from "@Constants";
+import { Tabs, LocationSelect } from "@Components";
 
 export const Aside = () => {
-  const { parameter } = useParameterData();
-
-  const currentTab = parameter || tabsData[0].tabId;
-  const isAirQualityParameter = parameter === "air_quality";
 
   return (
     <Flex
@@ -20,29 +13,6 @@ export const Aside = () => {
     >
       <LocationSelect />
       <Tabs />
-      <Flex
-        direction="column"
-        pr="11px"
-        gap="24px"
-        overflowY="auto"
-        sx={{
-          scrollbarWidth: "thin",
-          scrollbarColor: "white #48494D",
-          "&::-webkit-scrollbar": {
-            w: "4px",
-            bg: "gray.700",
-            borderRadius: "50px"
-          },
-          "&::-webkit-scrollbar-thumb": {
-            borderRadius: "50px",
-            bg: "white"
-          }
-        }}
-      >
-        <Cards cardsKey={currentTab} />
-        <IndexDate />
-        { isAirQualityParameter && <Indicators /> }
-      </Flex>
     </Flex>
   );
 };
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -6,9 +6,11 @@ import {
   TabPanel,
   TabPanels
 } from "@chakra-ui/react";
-import {resolveTranslationPath} from "@Helpers";
+import { resolveTranslationPath } from "@Helpers";
 import { useDataStore } from "@Store/useDataStore";
 import { tabsData } from "@Constants";
+import { Cards } from "@UI";
+import { IndexDate, Indicators } from "@Components";
 
 const hover = {
   color: "white"
@@ -21,6 +23,7 @@ const selected = {
 export const Tabs = () => {
   const { parameter, setParameter } = useDataStore();
 
+  const isAirQualityParameter = parameter === "air_quality";
   const tabs = tabsData.map((tabData: any) => resolveTranslationPath(tabData));
   const currentTab = parameter || tabs[0].tabId;
   const currentTabData = tabs.find(({ tabId }) => tabId === currentTab);
@@ -34,8 +37,9 @@ export const Tabs = () => {
     <ChakraTabs
       defaultIndex={defaultTabIndex}
       variant="unstyled"
+      overflowY="hidden"
     >
-      <TabList>
+      <TabList pb="24px">
         { tabs.map(({ tabId, tabName }) => (
           <Tab
             key={tabId}
@@ -53,12 +57,35 @@ export const Tabs = () => {
         )) }
       </TabList>
 
-      <TabPanels>
+      <TabPanels
+        height="100%"
+        overflowY="auto"
+        sx={{
+          scrollbarWidth: "thin",
+          scrollbarColor: "white #48494D",
+          "&::-webkit-scrollbar": {
+            w: "4px",
+            bg: "gray.700",
+            borderRadius: "50px"
+          },
+          "&::-webkit-scrollbar-thumb": {
+            borderRadius: "50px",
+            bg: "white"
+          }
+        }}
+      >
         { tabs.map(({ tabId }) => (
           <TabPanel
             key={tabId}
-            p="0"
-          />
+            display="flex"
+            flexDirection="column"
+            gap="24px"
+            p="0 11px 55px 0"
+          >
+            <Cards cardsKey={tabId} />
+            <IndexDate />
+            { isAirQualityParameter && <Indicators /> }
+          </TabPanel>
         )) }
       </TabPanels>
     </ChakraTabs>
