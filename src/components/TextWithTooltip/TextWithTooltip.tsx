e54473ba7f8e67cb2bcc808cf8ca91b6fc60a3b5
Oryngali <oryngali96@gmail.com>
2023-09-07 21:23:12 +0500

    [FE-224]: change tooltip placement for mobile

--- src/components/TextWithTooltip/TextWithTooltip.tsx
+++ src/components/TextWithTooltip/TextWithTooltip.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { Center, Flex, Tooltip, Text } from "@chakra-ui/react";
+import { useDetectWidth } from "@Hooks";
 import { ReactComponent as InfoFill } from "@Assets/icons/filled/harm-info-fill.svg";
 
 type TextWithTooltipProps = {
@@ -10,12 +11,16 @@ type TextWithTooltipProps = {
 }
 
 export const TextWithTooltip: FC<TextWithTooltipProps> = ({ label, text, fontSize, iconSize }) => {
+  const { isLargerThan600 } = useDetectWidth();
+
+  const placement = isLargerThan600 ? "right-start" : "bottom-end";
 
   return (
     <Tooltip
+      maxW={{ base: "343px", md: "600px"}}
       label={label}
       hasArrow
-      placement="right-start"
+      placement={placement}
       shouldWrapChildren
     >
       <Flex
