7cf0f9d1070bf405bdb3eafb4feafedbbb015769
gitquil <gitquil07@gmail.com>
2023-09-14 12:30:52 +0500

    [FE-231]: use Trans instead of t function

--- src/components/Footer/FooterContent.tsx
+++ src/components/Footer/FooterContent.tsx
@@ -1,11 +1,19 @@
 import { Text, Link as ChakraLink } from "@chakra-ui/react";
-import { t } from "i18next";
+import { Trans } from "react-i18next";
 import { useIncludeHover } from "@Hooks";
 
 export const FooterContent = () => {
   const shouldIncludeHover = useIncludeHover();
 
-  const textEnding = t("pages.footer.end");
+  const Link = <ChakraLink
+    href="https://hydromet.uz/"
+    isExternal
+    sx={
+      shouldIncludeHover
+        ? { _hover: { color: "yellow.500" } }
+        : { color: "yellow.500" }
+    }
+  />;
 
   return (
     <Text
@@ -20,23 +28,10 @@ export const FooterContent = () => {
         md: "initial"
       }}
     >
-      { t("pages.footer.start") }{ " " }
-      <ChakraLink
-        href="https://hydromet.uz/"
-        isExternal
-        sx={
-          shouldIncludeHover ?
-          { _hover: { color: "yellow.500" } } :
-          { color: "yellow.500" }
-        }
-      >
-        { t("pages.footer.link") }
-      </ChakraLink>
-      { textEnding && (
-        <>
-          { " " }{ textEnding }
-        </>
-      ) }
+      <Trans
+        i18nKey="pages.footer.sourceInfoText"
+        components={{ 1: Link }}
+      />
     </Text>
   );
 };
