d2d315c0195f3ac7c16b0077eb4073382d3b1a0c
Oryngali <oryngali96@gmail.com>
2023-09-15 14:41:08 +0500

    [FE-224]: decompose Cards component into small components

---
+++ src/_UI/Card/CardTextContent.tsx
@@ -0,0 +1,44 @@
+import { FC, useState } from "react";
+import { Text } from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
+import { getCutText } from "@Helpers";
+import { LETTERS_LIMIT } from "@Constants";
+import { CardResizerButton } from "./CardExpanderButton";
+
+type CardTextContentProps = {
+  subheading: string;
+  text: string;
+}
+
+export const CardTextContent: FC<CardTextContentProps> = ({ subheading, text }) => {
+  const [isCardOpen, setIsCardOpen] = useState(false);
+  const { t } = useTranslation();
+
+  const { isTextCut, cutText } = getCutText(text, LETTERS_LIMIT);
+  const cardText = isCardOpen ? text : cutText;
+  const cardButtonText = t(isCardOpen ? "pages.map.cards.buttons.hide" : "pages.map.cards.buttons.see_more");
+
+  const toggleCard = () => {
+    setIsCardOpen((prevState) => !prevState);
+  };
+
+  return (
+    <>
+      <Text
+        fontWeight="700"
+        casing="uppercase"
+        fontSize={{ base: "tiny", lg: "small" }}
+        lineHeight={{ base: "tiny", lg: "small" }}
+      >
+        { subheading }
+      </Text>
+      <Text
+        fontSize={{ base: "small", lg: "medium" }}
+        lineHeight={{ base: "small", lg: "medium" }}
+      >
+        { cardText }
+        { isTextCut && <CardResizerButton toggleCard={toggleCard} text={cardButtonText} /> }
+      </Text>
+    </>
+  );
+};
--- src/_UI/Card/Cards.tsx
+++ src/_UI/Card/Cards.tsx
@@ -1,15 +1,14 @@
 import { useState, FC } from "react";
-import { Box, Text, Button } from "@chakra-ui/react";
-import { t } from "i18next";
+import { Box } from "@chakra-ui/react";
 import { Card } from "./Card";
-import { LETTERS_LIMIT } from "@Constants";
-import { getCardData, getCutText } from "@Helpers";
+import { getCardData } from "@Helpers";
 import { useParameterData } from "@Hooks";
-import { ReactComponent as RightArrowIcon } from "@Assets/icons/stroke/harm-arrow-right.svg";
-import { ReactComponent as LeftArrowIcon } from "@Assets/icons/stroke/harm-arrow-left.svg";
 import { CardStep } from "./CardStep";
 import { CardNavigation } from "./CardNavigation";
 import { CardNavigationBox } from "./CardNavigationBox";
+import { CardTextContent } from "./CardTextContent";
+import { ReactComponent as RightArrowIcon } from "@Assets/icons/stroke/harm-arrow-right.svg";
+import { ReactComponent as LeftArrowIcon } from "@Assets/icons/stroke/harm-arrow-left.svg";
 
 type CardsType = {
   cardsKey?: any;
@@ -17,33 +16,26 @@ type CardsType = {
 
 export const Cards: FC<CardsType> = ({ cardsKey }) => {
   const [cardIndex, setCardIndex] = useState(0);
-  const [isCardOpen, setIsCardOpen] = useState(false);
   const { currentParameterValue } = useParameterData();
 
   const card = getCardData(currentParameterValue, cardsKey);
   const { heading, subheading, tip, text, cardColor, iconName } = card;
-  const { isTextCut, cutText } = getCutText(text, LETTERS_LIMIT);
-  const cardText = isCardOpen ? text : cutText;
-  const btnText = t(isCardOpen ? "pages.map.cards.buttons.hide" : "pages.map.cards.buttons.see_more");
 
   const cards = [card];
   const isNavigation = cards.length > 1;
   const isLeftActive = cardIndex > 0;
   const isRightActive = cardIndex < cards.length - 1;
+  const cardStep = `${cardIndex + 1} / ${cards.length}`;
 
   const handleClick = (value: number) => {
     setCardIndex((previous: number) => previous + value);
-  };
-
-  const toggleCard = () => {
-    setIsCardOpen((prevState) => !prevState);
   };
 
   return (
     <Box position="relative">
       { isNavigation && (
         <CardStep>
-          { cardIndex + 1 } / { cards.length }
+          { cardStep }
         </CardStep>
       ) }
       <Card
@@ -53,43 +45,24 @@ export const Cards: FC<CardsType> = ({ cardsKey }) => {
         iconName={iconName}
         height={isNavigation ? "290px" : "auto"}
       >
-        <Text
-          fontWeight="700"
-          casing="uppercase"
-          fontSize={{ base: "tiny", lg: "small" }}
-          lineHeight={{ base: "tiny", lg: "small" }}
-        >
-          { subheading }
-        </Text>
-        <Text
-          fontSize={{ base: "small", lg: "medium" }}
-          lineHeight={{ base: "small", lg: "medium" }}
-        >
-          { cardText }
-          { isTextCut && (
-            <>
-              { " " }
-              <Button variant="link" onClick={toggleCard}>
-                { btnText }
-              </Button>
-            </>
-          ) }
-        </Text>
+        <CardTextContent subheading={subheading} text={text} />
       </Card>
-      { isNavigation && (
-        <CardNavigation>
-          <CardNavigationBox justifyContent="flex-start">
-            { isLeftActive && (
-              <LeftArrowIcon onClick={() => handleClick(-1)} />
-            ) }
-          </CardNavigationBox>
-          <CardNavigationBox justifyContent="flex-end">
-            { isRightActive && (
-              <RightArrowIcon onClick={() => handleClick(+1)} />
-            ) }
-          </CardNavigationBox>
-        </CardNavigation>
-      ) }
+      {
+        isNavigation && (
+          <CardNavigation>
+            <CardNavigationBox justifyContent="flex-start">
+              { isLeftActive && (
+                <LeftArrowIcon onClick={() => handleClick(-1)} />
+              ) }
+            </CardNavigationBox>
+            <CardNavigationBox justifyContent="flex-end">
+              { isRightActive && (
+                <RightArrowIcon onClick={() => handleClick(+1)} />
+              ) }
+            </CardNavigationBox>
+          </CardNavigation>
+        )
+      }
     </Box>
   );
 };
