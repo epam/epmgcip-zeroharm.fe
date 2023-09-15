400cae1fb8cbe34a5d20876fc0c2a78222664bae
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-09-15 23:44:40 +0600

    [FE-229]: replace useColorMode with useColorModeValue

--- src/components/FeedbackButton/FeedbackButtonMobile.tsx
+++ src/components/FeedbackButton/FeedbackButtonMobile.tsx
@@ -1,16 +1,14 @@
 import { FC } from "react";
-import { IconButton, useColorMode } from "@chakra-ui/react";
+import { IconButton, useColorModeValue } from "@chakra-ui/react";
 import { useDataStore } from "@Store/useDataStore";
 import { ReactComponent as ThumbIcon } from "@Assets/icons/stroke/feedback-button-thumb.svg";
 
 export const FeedbackButtonMobile: FC = () => {
   const { toggleIsFormModalActive } = useDataStore();
 
-  const { colorMode } = useColorMode();
-
   const colorDark = "linear-gradient(129deg, #394 -18.95%, #FFA01C 22.54%, #FC7753 64.03%, #E6484E 105.52%, #C53446 147%, #7D5BA6 188.49%)";
   const colorLight = "linear-gradient(314deg, #3060E5 20.59%, #7CD0FF 115.69%)";
-  const color = colorMode === "dark" ? colorDark : colorLight;
+  const color = useColorModeValue(colorLight, colorDark);
 
   return (
     <IconButton
