141539b5a90b07a6ee11d0343b07c6754be0f696
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-09-17 09:48:57 +0600

    [FE-229]: use semantic tokens to change colorMode

--- src/components/FeedbackButton/FeedbackButtonMobile.tsx
+++ src/components/FeedbackButton/FeedbackButtonMobile.tsx
@@ -1,14 +1,10 @@
 import { FC } from "react";
-import { IconButton, useColorModeValue } from "@chakra-ui/react";
+import { IconButton } from "@chakra-ui/react";
 import { useDataStore } from "@Store/useDataStore";
 import { ReactComponent as ThumbIcon } from "@Assets/icons/stroke/feedback-button-thumb.svg";
 
 export const FeedbackButtonMobile: FC = () => {
   const { toggleIsFormModalActive } = useDataStore();
-
-  const colorDark = "linear-gradient(129deg, #394 -18.95%, #FFA01C 22.54%, #FC7753 64.03%, #E6484E 105.52%, #C53446 147%, #7D5BA6 188.49%)";
-  const colorLight = "linear-gradient(314deg, #3060E5 20.59%, #7CD0FF 115.69%)";
-  const color = useColorModeValue(colorLight, colorDark);
 
   return (
     <IconButton
@@ -21,8 +17,8 @@ export const FeedbackButtonMobile: FC = () => {
       borderRadius="50px"
       width="56px"
       height="56px"
-      bg={`${color}`}
-      _hover={{ bg: `${color}` }}
+      bg="feedbackButtonMobileColor"
+      _hover={{ bg: "feedbackButtonMobileColor" }}
       onClick={() => toggleIsFormModalActive()}
     />
   );
--- src/theme/foundations/semanticTokens.ts
+++ src/theme/foundations/semanticTokens.ts
@@ -1,12 +1,8 @@
 export const semanticTokens = {
-  semanticTokens: {
-    colors: {
-      0: "green.500",
-      6: "yellow.500",
-      11: "orange.500",
-      16: "red.500",
-      21: "ruby.500",
-      31: "violet.500"
+  colors: {
+    feedbackButtonMobileColor: {
+      default: "linear-gradient(314deg, #3060E5 20.59%, #7CD0FF 115.69%)",
+      _dark: "linear-gradient(129deg, #394 -18.95%, #FFA01C 22.54%, #FC7753 64.03%, #E6484E 105.52%, #C53446 147%, #7D5BA6 188.49%)"
     }
   }
 };
