ce7f4930e0b22a90913da1119ab954797d2939c0
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-09-18 14:48:15 +0600

    [FE-229]: change default color in semantic tokens

--- src/theme/foundations/semanticTokens.ts
+++ src/theme/foundations/semanticTokens.ts
@@ -1,8 +1,8 @@
 export const semanticTokens = {
   colors: {
     feedbackButtonMobileColor: {
-      default: "linear-gradient(314deg, #3060E5 20.59%, #7CD0FF 115.69%)",
-      _dark: "linear-gradient(129deg, #394 -18.95%, #FFA01C 22.54%, #FC7753 64.03%, #E6484E 105.52%, #C53446 147%, #7D5BA6 188.49%)"
+      default: "linear-gradient(129deg, #394 -18.95%, #FFA01C 22.54%, #FC7753 64.03%, #E6484E 105.52%, #C53446 147%, #7D5BA6 188.49%)",
+      _light: "linear-gradient(314deg, #3060E5 20.59%, #7CD0FF 115.69%)"
     }
   }
 };
