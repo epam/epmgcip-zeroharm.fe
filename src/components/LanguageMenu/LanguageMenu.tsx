3c7eaec63cd88028adb66049f3eb8fa8c8cb8a0b
Oryngali <oryngali96@gmail.com>
2023-09-19 13:22:23 +0500

    [FE-247]: language subject name adding to locales and create mobile menu heading tip with chakra factory

--- src/components/LanguageMenu/LanguageMenu.tsx
+++ src/components/LanguageMenu/LanguageMenu.tsx
@@ -1,11 +1,29 @@
 import { FC } from "react";
-import { Menu, Flex, useDisclosure } from "@chakra-ui/react";
+import { chakra, Menu, Flex, useDisclosure } from "@chakra-ui/react";
 import { useDetectWidth, useScreenScrollController } from "@Hooks";
 import { LanguageMenuList } from "./LanguageMenuList";
 import { LanguageMenuButton } from "./LanguageMenuButton";
+import { useTranslation } from "react-i18next";
+
+const MobileHeadingTip = chakra(Flex, {
+  baseStyle: {
+    w: "calc(100vw - var(--headerMobileHeight))",
+    h: "var(--headerMobileHeight)",
+    zIndex: "1",
+    bgColor: "secondaryBgColor",
+    color: "inherit",
+    pl: "8px",
+    pos: "fixed",
+    top: "0",
+    left: "var(--headerMobileHeight)",
+    alignItems: "center",
+    fontWeight: "bold"
+  }
+});
 
 export const LanguageMenu: FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { t } = useTranslation();
 
   const { isLargerThan600 } = useDetectWidth();
   const isMobileWidth = !isLargerThan600;
@@ -14,21 +32,9 @@ export const LanguageMenu: FC = () => {
   useScreenScrollController(isOpenOnMobile, !isOpenOnMobile);
 
   const mobileTitle = isOpenOnMobile && (
-    <Flex
-      w="calc(100vw - var(--headerMobileHeight))"
-      h="var(--headerMobileHeight)"
-      zIndex="1"
-      bgColor="secondaryBgColor"
-      color="inherit"
-      pl="8px"
-      pos="fixed"
-      top="0"
-      left="var(--headerMobileHeight)"
-      align="center"
-      fontWeight="bold"
-    >
-      Language
-    </Flex>
+    <MobileHeadingTip>
+      { t("lang.subject_name") }
+    </MobileHeadingTip>
   );
 
   return (
